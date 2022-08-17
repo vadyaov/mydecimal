decfb308d3cf96b9d02dbc1ac883c5ded41ca894
gloriagi <gloriagi@student.21-school.ru>
2022-08-18 00:48:31 +0300

    float-->dec test

--- src/tests/test_helpers.c
+++ src/tests/test_helpers.c
@@ -4,6 +4,13 @@ int get_rand_int(int min, int max) {
     return (rand() % (max - min + 1) + min);
 }
 
+float get_rand_float(float min, float max) {
+    // max always bigger than min
+    float rand = ((float)rand()) / (float)RAND_MAX;
+    floar range = max - min;
+    return rand * range + min;
+}
+
 s21_decimal random_decimal(int dec_size, int exp_size) {
     int exp = get_rand_int(0, exp_size);
     int sign = rand() % 2;
