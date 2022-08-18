58438f6b3a8b1704f423b374bf79e94f141668d3
Gloria <gloriagi@oa-e1.msk.21-school.ru>
2022-08-18 12:25:00 +0300

    1

--- src/main.c
+++ src/main.c
@@ -7,23 +7,31 @@
 
 int main () {
     s21_decimal a, b;
-    float A = -12.0;
-    float B = -3.0;
-    printf("round %.30f = %.30f\nfloor %.30f = %.30f\n", B, round(B), B, floorf(B));
-    printf("%.30f mod %.30f = %.30f\n", A, B, fmodf(A, B));
+    int count_ok = 0, count_not = 0;
+    float A = -9138.672456787213215545455;
+    for (; A < 465.3134354256565; A += 127.56351351) {
+    float B = A * (0.4567789789765451);
+
+    // printf("round %.30f = %.30f\nfloor %.30f = %.30f\n", B, round(B), B, floorf(B));
+    // printf("%.30f mod %.30f = %.30f\n", A, B, fmodf(A, B));
     s21_from_float_to_decimal(A, &a);
     s21_from_float_to_decimal(B, &b);
-    print_decimal(a);
-    print_decimal(b);
+    // print_decimal(a);
+    // print_decimal(b);
 
     s21_from_decimal_to_float(a, &A);
     s21_from_decimal_to_float(b, &B);
-    printf("%.30f %.30f\n", A, B);
+    // printf("%.30f %.30f\n", A, B);
     s21_decimal result = ZERO_DECIMAL;
-    s21_mod(a, b, &result);
-    print_decimal(result);
+    s21_mul(a, b, &result);
+    // print_decimal(result);
     float res;
     s21_from_decimal_to_float(result, &res);
     printf("%.30f\n", res);
+    printf("result: %.28f\n", A * B);
+    if (res == A * B) count_ok++;
+    else count_not++;
+    }
+    printf("%d\n%d", count_ok, count_not);
     return 0;
 }
